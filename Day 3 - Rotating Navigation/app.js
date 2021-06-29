04577775ad3651019da3f9650f2336693e231217
ranjit kairi <ranjitkoiri009@gmail.com>
2021-06-29 15:21:37 +0530

    completed rotaiting navigation

---
+++ Day 3 - Rotating Navigation/app.js
@@ -0,0 +1,8 @@
+// Block Elements And Variables
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.querySelector(".container");
+
+// Add Event Listener
+open.addEventListener("click", () => container.classList.add("show-nav"));
+close.addEventListener("click", () => container.classList.remove("show-nav"));
