fe31137141e81a4c58f64343e0460c584f6f8a69
ranjit kairi <ranjitkoiri009@gmail.com>
2021-06-30 19:33:53 +0530

    completed form wave animation

---
+++ Day 8 - Form Wave Animation/app.js
@@ -0,0 +1,13 @@
+// Block Elements and Variables
+const labels = document.querySelectorAll(".form-control label");
+
+// forEach
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+    .join("");
+});
