26197f2a843da1a98a63627f4096fd74921d47d9
ranjit kairi <ranjitkoiri009@gmail.com>
2021-07-01 12:14:37 +0530

    completed dad jokes

---
+++ Day 10 - Dad Jokes/app.js
@@ -0,0 +1,38 @@
+// Block Elements and Variables
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
+
+
+// Functions 1....async/await
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const res = await fetch("https://icanhazdadjoke.com", config);
+
+  const data = await res.json();
+
+  jokeEl.innerHTML = data.joke;
+}
+
+// Functions 2.... using.then();
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+
+//   fetch("https://icanhazdadjoke.com", config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
